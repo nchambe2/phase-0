a02dd542c17b4e38b19f9a1a0433ba125fc41b06
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-11 17:57:03 -0600

    Update variables-methods.rb

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -33,11 +33,16 @@ puts 'I know you said your favorite number is ' + ' ' + favorite_number.to_s + '
 #https://github.com/nchambe2/phase-0/blob/master/week-4/math/my_solution.rb
 
 #How do you define a local variable?
-#In order to define a local variable you set a variable name equal to a single value. Local variable names can contain any sequence of characters, but the first letter must be lower case.
+#In order to define a local variable you set a variable name equal to a single value. 
+#Local variable names can contain any sequence of characters, but the first letter must be lower case.
 #For example: x = "a"
 
 #How do you define a method?
-# In order to define a method you have to use the keyword def, then the method name, then any parameters you plan to pass to the method, and finally close the definition by placing the word end. In between the def and the end keyword is where you place specific actions you want the method to perform.
+# In order to define a method you have to use the keyword def.
+# Then you add the method name. 
+# Next to the method name you can include parameters that you plan to pass to the method. 
+# Finally, you need to close the definition by placing the keyword end. 
+# In between the def and the end keyword is where you place specific actions you want the method to perform.
 
 #For example:
 #def say_my_name(name)
@@ -45,7 +50,10 @@ puts 'I know you said your favorite number is ' + ' ' + favorite_number.to_s + '
 #end
 
 #What is the difference between a local variable and a method?
-#Methods are resuable chunks of code that carry out specific tasks. While local variables are containers for single values that you want to hold on to for later use. While local variables don't actually do anything in of themselves you can query them in order to get information about what is stored inside of the variable. Also, methods can perform actions on local variables.
+#Methods are resuable chunks of code that carry out specific tasks. 
+#While local variables are containers for single values that you want to hold on to for later use. 
+#While local variables don't actually do anything in of themselves you can query them in order to get information about what is stored inside of the variable. 
+#Also, methods can perform actions on local variables.
 
 #How do you run a ruby program from the command line?
 #In order to run a ruby program from the command line you have to type ruby file_name.rb
@@ -54,7 +62,8 @@ puts 'I know you said your favorite number is ' + ' ' + favorite_number.to_s + '
 #In order to run a rspec file from the command line you have to type rspec file_name_spec.rb
 
 #What was confusing about this material? What made sense?
-#I don't think the material was confusing. I thought it was a great opportunity to get more practice with Ruby, and spend some time reinforcing the core concepts.
+#I don't think the material was confusing. 
+#I thought it was a great opportunity to get more practice with Ruby, and spend some time reinforcing the core concepts.
 
 
 
