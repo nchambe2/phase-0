a1c80637f419c1aa3f4a831c79f4b3a1009ecd38
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-11 17:47:59 -0600

    Answer reflection questions

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -23,5 +23,31 @@ bigger_better_number = favorite_number + 1
 puts 'I know you said your favorite number is ' + ' ' + favorite_number.to_s + ' , but this number: ' +
       bigger_better_number.to_s + ' is a even bigger better number.'
 
+#How do you define a local variable?
+#In order to define a local variable you set a variable name equal to a single value. Local variable names can contain any sequence of characters, but the first letter must be lower case.
+#For example: x = "a"
+
+#How do you define a method?
+# In order to define a method you have to use the keyword def, then the method name, then any parameters you plan to pass to the method, and finally close the definition by placing the word end. In between the def and the end keyword is where you place specific actions you want the method to perform.
+
+#For example:
+#def say_my_name(name)
+  #block of code
+#end
+
+#What is the difference between a local variable and a method?
+#Methods are resuable chunks of code that carry out specific tasks. While local variables are containers for single values that you want to hold on to for later use. While local variables don't actually do anything in of themselves you can query them in order to get information about what is stored inside of the variable. Also, methods can perform actions on local variables.
+
+#How do you run a ruby program from the command line?
+#In order to run a ruby program from the command line you have to type ruby <file_name>.rb
+
+#How do you run an RSpec file from the command line?
+#In order to run a rspec file from the command line you have to type rspec <file_name_spec>.rb
+
+#What was confusing about this material? What made sense?
+#I don't think the material was confusing. I thought it was a great opportunity to get more practice with Ruby, and spend some time reinforcing the core concepts.
+
+
+
 
 
