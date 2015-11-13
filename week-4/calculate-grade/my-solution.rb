def05a82fda215541a349c11a51de2a9034b840d
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-13 14:49:36 -0600

    Create a method that calculates letter grades && gps-2-1.css file

---
+++ week-4/calculate-grade/my-solution.rb
@@ -0,0 +1,24 @@
+# Calculate a Grade
+
+def get_grade(average)
+
+  if average >= 90
+    p "A"
+  elsif average >= 80
+    p "B"
+  elsif average >= 70
+    p "C"
+  elsif average >= 60
+    p "D"
+  else average < 60
+    p "F"
+  end
+end
+
+#get_grade(100)
+
+
+# I worked on this challenge [with: Mason Pierce].
+
+
+# Your Solution Below
