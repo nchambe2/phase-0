ce4b65684cdce34201144e58087c4fbeda0fee0a
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-13 15:24:01 -0600

    Add pseudocode to the get_grade method solution

--- week-4/calculate-grade/my-solution.rb
+++ week-4/calculate-grade/my-solution.rb
@@ -1,20 +1,22 @@
 # Calculate a Grade
 
-def get_grade(average)
+=begin
+  INPUT: Obtain the average
 
-  if average >= 90
-    p "A"
-  elsif average >= 80
-    p "B"
-  elsif average >= 70
-    p "C"
-  elsif average >= 60
-    p "D"
-  else average < 60
-    p "F"
-  end
-end
+  IF average is >= 90 THEN
+    return a letter grade of A
+  IF average is >= 80 THEN
+    return a letter grade of B
+  IF average is >= 70 THEN
+    return a letter grade of C
+  IF average is >= 60 THEN
+    return a letter grade of D
+  IF average is < 60 THEN
+    return a letter grade of F
+  END IF
 
+  OUTPUT: Return the corresponding letter grade to that average
+=end
 #get_grade(100)
 
 
@@ -22,3 +24,18 @@ end
 
 
 # Your Solution Below
+
+def get_grade(average)
+
+  if average >= 90
+    return "A"
+  elsif average >= 80
+    return "B"
+  elsif average >= 70
+    return "C"
+  elsif average >= 60
+    return "D"
+  else average < 60
+    return "F"
+  end
+end
