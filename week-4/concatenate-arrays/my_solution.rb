4f30624742fa990238ac0773c5a18a4b7de70916
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-14 12:10:55 -0600

    Create a method that concatenates arrays

---
+++ week-4/concatenate-arrays/my_solution.rb
@@ -0,0 +1,18 @@
+# Concatenate Two Arrays
+=begin
+
+INPUT: Obtain two collections of words
+
+If both collections are empty THEN
+    return the empty collection
+ELSE
+OUTPUT: Combine the two collections of words into one
+=end
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+def array_concat(array_1, array_2)
+
+end
