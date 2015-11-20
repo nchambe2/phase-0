7b01ea8f4fc9bf6fe4343aa8fa7e6454c941ea0f
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-20 03:20:29 -0600

    Solve seperate comma challenge

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -34,22 +34,33 @@ OUTPUT: Return a comman seperated string
 =end
 
 # 1. Initial Solution
-=begin
+#begin
+
 def separate_comma(number)
   number = number.to_s
+  add_comma = []
+  add_another_comma = []
 
     if number.length < 4
-        return number
-    elsif number.length >= 4 && number.length <= 6
-         number.insert(-4, ",")
          return number
+    elsif number.length >= 4 && number.length <= 6
+          reverse_string = number.reverse
+          1.times do
+            add_comma = reverse_string[2] + ","
+          end
+          reverse_string[2] = add_comma
+        return reverse_string.reverse
     elsif number.length >= 7 && number.length <=8
-          number.insert(-4, ",") && number.insert(-8, ",")
-          return number
+          reverse_string = number.reverse
+          2.times do
+            add_comma = reverse_string[2] + ","
+            add_another_comma = reverse_string[5] + ","
+          end
+          reverse_string[2] = add_comma
+          reverse_string[6] = add_another_comma
+        return reverse_string.reverse
     end
 end
-=end
-
 
 # 2. Refactored Solution
 def separate_comma(number)
@@ -67,26 +78,27 @@ end
 
 # 3. Reflection
 =begin
-What was your process for breaking the problem down? What different approaches did you consider?
-So I really over thought this problem. My first approach was centered on turning the number parameter into a array, reversing the array, adding a comma after index three, undoing the array, and then return the string.
+So I really over thought this problem.
+I started out with very vague pseudcode.
+Which meant that I was trying to solve a problem that I didn't fully understand.
 
-After a hour and half of that I was pretty frustrated. I had to take a break. When I came back I scrapped the whole thing, and started fresh.
+So I looked at the specs more,and realized that there was a pattern in terms of comma position and number length.
 
-One big thing I noticed was that my pseudcode was very vague. Which meant that I was trying to solve a problem that I didn't fully understand.
+When I noticed that I went back and re-pseudocoded my solution.
 
-I looked at the specs more and realized that there was a pattern in terms of comma position and number length.
+I broke the problem down into three different conditional expressions.
 
-When I notice that I went back and started pseudo coding. Once I was done with that I implemented my solution. All in all that took me around 30 so minutes.
+The first conditional branch was to return the original array if the length of the number was less than 4.
 
+The second conditional branch add one comma to numbers between 4 and 6.
+
+Finally, the last conditional branch added two commas to numbers between 7 and 8.
 I find pseudo coding helpful because it helps me determine where my failure points are. If I pseudocode first and can't solve the problem then I know I don't understand it.
 
-I used the insert method for the refactored solution. I tried to stray away from methods that were to cryptic. Instead, choosing one that decreased the lengthen of my code, but didn't sacrifice readability.
+I initally reversed my string, iterated through the reversed string, added a comma(s), pushed the number with a comma into the array, then returned the reversed string.
 
+I used the insert method for my refactored solution. I tried to stray away from methods that were to cryptic. Instead, choosing one that decreased the length of my code, but didn't sacrifice readability.
 
+I do think that my code is more readable. I took out the explicit returns, shortened my conditional statements, and used a built in Ruby method to replace having to reverse my string and iterate through it to place the comma.
 
-I do think that my code is more readable. I took out some of the explicit returns along with shortning some of the conditional statements. I think conditionals can get really messy because it's so hard to follow what's going on.
-
-
-How did you initially iterate through the data structure?
-Do you feel your refactored solution is more readable than your initial solution? Why?
 =end
