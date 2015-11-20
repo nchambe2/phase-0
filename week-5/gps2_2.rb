373017864348e20a7583eef5610e4681551e2f95
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-20 01:41:35 -0600

    Update gps2_2.rb

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -84,18 +84,32 @@ end
 print_list(update_items_list)
 
 =begin
-Pseudocoding for me with others is hard because I always feel like the pseudocoding part takes me too long to complete. Generally, I rush that part with my pair, and then jump straight into coding. After we finished the challenge I went back and improved my pseudocode. I would love to know how much is a appropriate time to actually pseudocode with your pair.
+Pseudocoding for me with others is hard because I always feel like the pseudocoding part takes me too long to complete. 
+Generally, I rush that part with my pair, and then jump straight into coding. 
+After we finished the challenge I went back and improved my pseudocode. 
+I would love to know how much is a appropriate time to actually pseudocode with your pair.
 
-Arrays are great container objects for storing ordered items. While hashes are better for keeping track of non-orderd items. If I had chosen to use an array creating, adding,and deleting items from the list would have been pretty easy to do since there are a lot of built in array methods that do it for you.
+Arrays are great container objects for storing ordered items. 
+While hashes are better for keeping track of non-orderd items. 
+If I had chosen to use an array creating, adding,and deleting items from the list would have been pretty easy to do since there are a lot of built in array methods that do it for you.
 
-However, I chose to use a Hash because each item in the list corresponded with a certain amount. Using a hash made it easier to keep track of the amount per item. Doing that for an array would have been harder. Since items are arranged by order within the array, not based on value.
+However, I chose to use a Hash because each item in the list corresponded with a certain amount. 
+Using a hash made it easier to keep track of the amount per item. 
+Doing that for an array would have been harder. 
+Since items are arranged by order within the array, not based on value.
 
 Methods will return the last expression evaluated, unless an explicit return has been declared somewhere else in the method.
 
-You can declare parameter(s) in your method definitions. These parameters act as containers for values passed via method calls. Unlike other languages Ruby doesn't force you to declare the type of item that will be stored in a parameter, it just grasps on to what you give it. So you can pass integers, floats, strings, container objects (arrays and hashes), and other methods.
+You can declare parameter(s) in your method definitions. 
+These parameters act as containers for values passed via method calls. 
+Unlike other languages Ruby doesn't force you to declare the type of item that will be stored in a parameter, it just grasps on to what you give it. 
+So you can pass integers, floats, strings, container objects (arrays and hashes), and other methods.
 
-In other to pass information betwen methods you have to store the method call in a variable. You can then pass this variable as a argument to another method.
+In other to pass information betwen methods you have to store the method call in a variable. 
+You can then pass this variable as a argument to another method.
 
-Before this I didn't know how to pass inormation from one method to another. So I was excited to learn this concept. I'm very curious about if someone did choose to use an array, how they would go about creating a clean solution.
+Before this I didn't know how to pass inormation from one method to another. 
+So I was excited to learn this concept. 
+I'm very curious about if someone did choose to use an array, how they would go about creating a clean solution.
 
 =end
