e1b2ac542ed386f309ced6c1dd69f6137ea72498
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-21 10:56:54 -0600

    Refactor mode array to use select method

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,77 +1,89 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [with: Monique ]
 
-# I spent [] hours on this challenge.
+# I spent [1] day on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
 # is commented.
 
-
-
 # 0. Pseudocode
+=begin
+INPUT: Obtain a container object with a collection of values
+    IF all values are unique THEN
+        return the container object with all the values inside of it
+    IF there is only one most frequent value THEN
+        return the container object with the most frequenct value
+    IF there are multiple frequent values THEN
+        return the container object with a collection of most frequenct values
+    END IF
+OUTPUT: Return a container object with the most frequent value
+=end
+#1. define method -Mode
+#2. create array
+#3. method takes objects, strings, or integers
+#4. if all values are unique return array
+#5. most frequent value(s) returned
 
 # What is the input?
+#Obtain a container object with a collection of integers or words
 # What is the output? (i.e. What should the code return?)
+#return a container object with the most frequent value found in the collection
 # What are the steps needed to solve the problem?
-
 
 # 1. Initial Solution
 
-=begin
-include Enumerable
 def mode (arrays)
   most_frequent = Hash.new(0)
-  arrays.each { |arrays| most_frequent[arrays] += 1}
-  p most_frequent.sort_by { |key, value| value }
-
-end
-=end
-
-=begin
-
-include Enumerable
-def mode (arrays)
-  key = []
-  number = []
-  most_frequent = Hash.new(0)
-  arrays.each { |arrays| most_frequent[arrays] += 1}
-  # most_frequent.sort_by { |key, value| value }
-  p key = most_frequent.max_by{ |k,v| v}[0]
-  p number = key.to_s.split(" ")
-
-  number.each_with_index do |i, x|
-      p number[x] = i.to_i
-  end
-
-
-
-=end
-
-include Enumerable
-def mode (arrays)
-  most_frequent = Hash.new(0)
-  arrays.each do |i|
-    most_frequent[i] += 1
-  end
-  mode_arrays =[]
+  arrays.each {|i| most_frequent[i] += 1}
+  mode_arrays = []
   most_frequent.each do |k,v|
-    if v ==most_frequent.values.max
+    most_frequent_values = most_frequent.values.max
+    if v == most_frequent_values
       mode_arrays << k
+    end
+  end
+p mode_arrays.sort
 end
-end
-mode_arrays.sort
-end
 
 
-
-
-#mode(["apple", "orange", "apple"])
 
 # 3. Refactored Solution
-
-
-
+def mode (arrays)
+    frequent_value = []
+    most_frequent = Hash.new(0)
+    arrays.each { |array| most_frequent[array] += 1 }
+    most_frequent_values = most_frequent.values.max
+    most_frequent.select! { |key, value| frequent_value << key  if value == most_frequent_values }
+    frequent_value
+end
 
 # 4. Reflection
+
+=begin
+We decided to use a hash.
+The data that we were trying to hold: frequency of a value in a collection would be easier to store and access in a hash.
+By using a hash we converted each value in the array into a key.
+Then we assigned the frequency of the key to a value.
+
+From the start we knew that we wanted to convert the array into a Hash.
+So we immediately created a empty hash with a default value.
+We iterated over the array and stored each value as hash.
+We got stuck right there for a while.
+We couldn't figure out how to return the key with the max value, and convert the current data structure back into an array.
+We came back to it the next day and realized that:
+1.) We didn't have a variable that stored the max value
+2.) We also need to create a empty array that we could place the key associated with the most frequent value
+
+Once we added those two things to our solution we were able to compare each value to the most frequent value, capture those key value pairs then place the keys into a empty array.
+
+I think we struggled for a while because our pseudocode could have been stronger.
+We were so busy throwing Ruby methods at it that it didn't occur to us that maybe the reason we couldn't solve it was becaue we didn't understand the problem fully.
+Later on after we had solved the challenge I came back and refactored our pseudo code.
+It helped expose were our issues stemmed from.
+
+Originally we iteratated over the hash using each.
+While each worked perfectly fine using it made our original solution very verbose.
+For our refacted solution we decided to use select! because we only cared about keeping the key/value pairs with the most frequent values.
+=end
