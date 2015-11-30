8f0e1159afa55ad97ff4c1c2658de48809162e4b
Nicolette Chambers <nicolette3883@gmail.com>
2015-11-30 16:52:12 -0600

    Move group_proj_solution to group_proj folder

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,19 @@
+// As a user
+// I want to create a sum, median, and mode function that returns the correct value
+// So that I don’t have to do it manually
+
+// As a user
+// I want to have a function that returns an error message when an invalid test is passed
+// So that the user knows why the function did not execute properly
+
+// As a user
+// I want to have a function that returns the sum
+// So that I can easily calculate the total value of all the numbers in a odd or even number list
+
+// As a user
+// I want to have a function that returns the mean
+// So that I can easily calculate the average of all the numbers in a odd or even number list
+
+// As a user
+// I want to have a function that returns the median
+// So that I can easily calculate the middle value of all the numbers in a odd or even number list
