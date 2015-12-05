2933bb0752813b6dd06b1921a51bd3ef1fbfa83f
Nicolette Chambers <nicolette3883@gmail.com>
2015-12-05 12:56:57 -0600

    Add mean, median, and sum function to test.js file

--- week-7/group-project/test.js
+++ week-7/group-project/test.js
@@ -6,6 +6,50 @@
 oddLengthArray  = [1, 2, 3, 4, 5, 5, 7]
 evenLengthArray = [4, 4, 5, 5, 6, 6, 6, 7]
 
+// Note: Code was not refactored as it was already as DRY as it could be.
+// All tests passed execpt for median should return the median value of all elements in an array with an odd length.
+
+// Function 1
+// I as a user want to make sure that whenever I used this program, that if something goes wrong, the program will let me know. (And in more coder speak terms, that if the argument is not an array or if it is and has a nil value, then an error will be thrown.)
+
+function isArrayOfNum(arr) {
+  if(!Array.isArray(arr)) throw new Error("Input is not an array");
+  for(var i = 0; i < arr.length; i++) {
+    if(isNaN(arr[i])) throw new Error("Input is an array with at least 1 NaN value");
+  }
+  return arr;
+}
+
+// Function 2
+// I as a user want to know the sum of a list of numbers.
+function sum(arr) {
+  var runSum = 0;
+  for(var i = 0; i < arr.length; i++)
+    runSum += arr[i];
+  return runSum;
+}
+
+// Function 3
+// I as a user want to know the mean of a list of numbers.
+function mean(arr) {
+  var runSum = 0;
+  for(var i = 0; i < arr.length; i++)
+    runSum += arr[i];
+  return runSum / arr.length;
+}
+
+// Function 4
+// I as user want to know the mediam of a list of numbers.
+function median(arr) {
+  sortedArr = arr.sort(function(a, b) {
+    return a - b;
+  });
+  length = arr.length;
+  if (length % 2 == 1)
+    return arr[length/2];
+  else
+    return (arr[length/2] + arr[length/2 - 1]) / 2;
+}
 
 function assert(test, message, test_number) {
   if (!test) {
