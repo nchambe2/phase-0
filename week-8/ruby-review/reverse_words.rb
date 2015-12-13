960f50591e3e4c59905de202bfbe596c27d7c297
Nicolette Chambers <nchamber@thoughtworks.com>
2015-12-13 01:23:03 -0600

    Create method that reverses word

---
+++ week-8/ruby-review/reverse_words.rb
@@ -0,0 +1,40 @@
+# Reverse Words
+
+
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+=begin
+Obtain a word
+
+split words into an array
+
+iterate through each word
+   for every word reverse it
+
+join the array back into a string
+
+return reversed word
+=end
+
+# Initial Solution
+
+def reverse_words(words)
+  word_array = words.split(" ")
+  word_array.map! { |word| word.reverse}
+  word_array.join(" ")
+end
+
+
+# Refactored Solution
+
+def reverse_words(words)
+  word_array = words.split(" ")
+  word_array.map! { |word| word.reverse}.join(" ")
+end
+
+
+
+
+# Reflection
